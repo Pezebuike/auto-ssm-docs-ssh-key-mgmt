632fd74e48690dcc9f8dd455f8125bfaf89e5fe6
ezebuike michael <99094143+Pezebuike@users.noreply.github.com>
2025-06-21 10:36:04 +0100

    updated worflows

--- module/ssm-key-mgmt/backend.tf
+++ module/ssm-key-mgmt/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bucket"
+    bucket         = ""
     key            = "ssh-key-mgmt/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-north-1"
     dynamodb_table = "terraform-locks"
     
   }
